0d0c4c8b6bdbeb998cee1021de14d2307a5001b9
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-01-20 13:44:07 -0800

    submitting work to repo

--- scripts/repo.js
+++ scripts/repo.js
@@ -19,11 +19,11 @@
 
 //then replace this call with a $.get() call.
   repos.requestRepos = function(callback) {
-    $.ajax({
+    $.get({
       url: '/github/users/nadiabahrami/repos' +
          '?per_page=100' + '&sort=updated',
-       type: 'GET',
-         success: function(data, message, xhr) {
+      //  type: 'GET',
+    }).done(function(data, message, xhr) {
         repos.all = data;
       }
     }).done(callback);
